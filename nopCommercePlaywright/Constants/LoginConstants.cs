ca0d737ccc42066acc63a8804dd014dd6efa06a5
Ilse Macías <ilse_macias@hotmail.com>
2022-06-02 15:45:52 -0700

    Remove using from the classes.

--- nopCommercePlaywright/Constants/LoginConstants.cs
+++ nopCommercePlaywright/Constants/LoginConstants.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace nopCommercePlaywright.Constants
+﻿namespace nopCommercePlaywright.Constants
 {
-    internal class LoginConstants
+    public static class LoginConstants
     {
     }
 }
